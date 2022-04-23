c9037885acced700886381078676b0b91ae2b1d1
andremawad0 <andremawad0@gmail.com>
2022-04-23 17:02:48 +0300

    feat:add password modal

--- lib/screens/edit_profile/components/password_form.dart
+++ lib/screens/edit_profile/components/password_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smartfit/screens/edit_profile/components/change_password_button.dart';
 import 'package:smartfit/screens/edit_profile/components/text_field_input.dart';
 
 class PasswordForm extends StatefulWidget {
@@ -6,33 +7,85 @@ class PasswordForm extends StatefulWidget {
   State<PasswordForm> createState() => _PasswordFormState();
 }
 
+late String oldPassword;
+late String newPassword;
+late String confirmNewPassword;
+
 class _PasswordFormState extends State<PasswordForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Align(
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  children: <Widget>[
-                    Text(
-                      "New Password",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                      ),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: const <Widget>[
+                  Text(
+                    "Old Password",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
                     ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 5.0),
-            TextFieldInput(onChanged: (value) {}),
-          ],
-        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 5.0),
+          TextFieldInput(onChanged: (value) {
+            setState(() {
+              newPassword = value;
+            });
+          }),
+          const SizedBox(height: 14),
+          Row(
+            children: [
+              Column(
+                children: const <Widget>[
+                  Text(
+                    "New Password",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 5.0),
+          TextFieldInput(onChanged: (value) {
+            setState(() {
+              newPassword = value;
+            });
+          }),
+          const SizedBox(height: 14),
+          Row(
+            children: [
+              Column(
+                children: const <Widget>[
+                  Text(
+                    "Confirm New Password",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 5.0),
+          TextFieldInput(onChanged: (value) {
+            setState(() {
+              newPassword = value;
+            });
+          }),
+          const SizedBox(height: 10.0),
+          ChangePasswordButton()
+        ],
       ),
     );
   }
